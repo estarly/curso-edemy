3a1e747bf9c9374c439139758d6da53e4a5ae7ba
Carl Lewis Castillo <carllewiscastillo@gmail.com>
2025-04-10 15:05:50 -0400

    Update page.jsx

--- src/app/instructor/course/[courseId]/lessons/page.jsx
+++ src/app/instructor/course/[courseId]/lessons/page.jsx
@@ -3,6 +3,7 @@ import { getCourseById } from "@/actions/getCourseById";
 import Header from "../../Header";
 import CourseLessons from "@/components/Instructor/CourseLessons";
 import DeleteButton from "./DeleteButton";
+import EditButton from "./_components/EditButton";
 
 const Page = async ({ params }) => {
 	const { course, videos } = await getCourseById(params);
@@ -20,29 +21,30 @@ const Page = async ({ params }) => {
 						<hr />
 						<div className="row">
 							{videos.map((vdo) => (
-								<>
-									<div className="col-md-3" key={vdo.id}>
-										<div className="card">
-											<video width="100%" controls>
+								<div className="col-md-3" key={vdo.id}>
+									<div className="card h-100">
+										<div className="video-container" style={{ height: "160px", overflow: "hidden" }}>
+											<video width="100%" height="100%" style={{ objectFit: "cover" }} controls>
 												<source
 													src={vdo.video_url}
 													type="video/mp4"
 												/>
-												Your browser does not support
-												HTML video.
+												Tu navegador no soporta
+												videos HTML.
 											</video>
+										</div>
 
-											<div className="card-body">
-												<h6 className="card-title">
-													{vdo.title}
-												</h6>
-												<DeleteButton
-													videoId={vdo.id}
-												/>
+										<div className="card-body d-flex flex-column justify-content-between">
+											<h6 className="card-title text-truncate">
+												{vdo.title}
+											</h6>
+											<div>
+												<EditButton videoId={vdo.id} />
+												<DeleteButton videoId={vdo.id} />
 											</div>
 										</div>
 									</div>
-								</>
+								</div>
 							))}
 						</div>
 					</div>
